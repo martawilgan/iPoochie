20181decf14cc6b5ab44c2b767362438bbac35d4
marta wilgan <mmw319@nyu.edu>
2013-05-05 08:20:12 -0400

    implementing eatViewController

--- iPoochie/EatViewController.h
+++ iPoochie/EatViewController.h
@@ -7,14 +7,31 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <CoreMotion/CoreMotion.h>
+#import <AudioToolbox/AudioToolbox.h>
 
-@interface EatViewController : UIViewController
+#define kAccelerationThreshold      1.7
+#define kUpdateInterval             (1.0f / 10.0f)
 
-@property (weak, nonatomic) IBOutlet UILabel *pointsLabel;
+@interface EatViewController : UIViewController <UIAccelerometerDelegate>
+
 @property (strong, nonatomic) NSNumber *points;
-
+@property (weak, nonatomic) IBOutlet UILabel *pointsLabel;
+@property (weak, nonatomic) IBOutlet UILabel *infoBubbleLabel;
 @property (weak, nonatomic) IBOutlet UIButton *backButton;
+@property (weak, nonatomic) IBOutlet UIImageView *bowlImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *petImageView;
+@property (weak, nonatomic) IBOutlet UIImageView *infoBubbleImageView;
+
+@property (assign, nonatomic) BOOL fullBowl;
+@property (strong, nonatomic) CMMotionManager *motionManager;
+@property (strong, nonatomic) UIImage *empty;
+@property (strong, nonatomic) UIImage *full;
+@property (strong, nonatomic) NSArray *shakes;
+
+
 
 -(IBAction)goBack: (id)sender;
+-(IBAction)eat:(id)sender;
 
 @end
--- iPoochie/EatViewController.m
+++ iPoochie/EatViewController.m
@@ -16,6 +16,17 @@
 @implementation EatViewController
 @synthesize points;
 @synthesize pointsLabel;
+@synthesize bowlImageView;
+@synthesize petImageView;
+@synthesize motionManager;
+@synthesize fullBowl;
+@synthesize full;
+@synthesize empty;
+@synthesize shakes;
+@synthesize infoBubbleImageView;
+@synthesize infoBubbleLabel;
+
+int gIndex = 0;
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
@@ -30,6 +41,80 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    // Set the images
+    self.empty = [UIImage imageNamed:@"Shake0.gif"];
+    self.full = [UIImage imageNamed:@"Shake9.gif"];
+    
+    // Make bowl empty
+    bowlImageView.image = empty;
+    
+    // Create the shakes array
+    NSArray *shakesArray = [NSArray arrayWithObjects:
+                            [UIImage imageNamed:@"Shake1.gif"],
+                            [UIImage imageNamed:@"Shake2.gif"],
+                            [UIImage imageNamed:@"Shake3.gif"],
+                            [UIImage imageNamed:@"Shake4.gif"],
+                            [UIImage imageNamed:@"Shake5.gif"],
+                            [UIImage imageNamed:@"Shake6.gif"],
+                            [UIImage imageNamed:@"Shake7.gif"],
+                            [UIImage imageNamed:@"Shake8.gif"],
+                            [UIImage imageNamed:@"Shake9.gif"],
+                            nil];
+    self.shakes = shakesArray;
+    
+    gIndex = 0; // current index of the shakes array
+    
+    // Shakes motion - fill up bowl while shake detected
+    self.motionManager = [[CMMotionManager alloc] init];
+    motionManager.accelerometerUpdateInterval = kUpdateInterval;
+    NSOperationQueue *queue = [[NSOperationQueue alloc] init];
+    [motionManager startAccelerometerUpdatesToQueue:queue
+                                        withHandler:
+     ^(CMAccelerometerData *accelerometerData, NSError *error){
+         if(error)
+         {
+             [motionManager stopAccelerometerUpdates];
+         }
+         else
+         {
+             if(!fullBowl)
+             {
+                 CMAcceleration acceleration = accelerometerData.acceleration;
+                 
+                 if(acceleration.x > kAccelerationThreshold
+                    || acceleration.y > kAccelerationThreshold
+                    || acceleration.z > kAccelerationThreshold)
+                 {
+                     // Change image for shake
+                     if(gIndex <= 8 )
+                     {
+                         // Make bubble hidden
+                         if(gIndex == 0)
+                         {
+                             [self performSelectorOnMainThread:@selector(hideBubble:) withObject:nil waitUntilDone:NO];
+                         }
+                         
+                         // Change bowl image view
+                         [bowlImageView performSelectorOnMainThread:@selector(setImage:)
+                                                         withObject:shakes[gIndex]
+                                                      waitUntilDone:NO];
+                         gIndex++;
+                     }
+                     else
+                     {
+                         fullBowl = YES;
+                         [bowlImageView performSelectorOnMainThread:@selector(setImage:)
+                                                         withObject:full
+                                                      waitUntilDone:NO];
+                     }
+                         
+                 }
+                 
+             }
+         }
+     }];
+    
 }
 
 -(void) viewDidAppear:(BOOL)animated
@@ -53,10 +138,115 @@
     // Dispose of any resources that can be recreated.
 }
 
+-(void) viewDidUnload
+{
+    [super viewDidUnload];
+    self.bowlImageView = nil;
+    self.motionManager = nil;
+    self.empty = nil;
+    self.full = nil;
+    
+}
+
+// Hide bubble image and label
+-(void)hideBubble:(NSString *)string
+{
+    [self.infoBubbleImageView setHidden:YES];
+    [self.infoBubbleLabel setHidden:YES];
+}
+
 // Go back to interactViewController
 -(IBAction)goBack: (id)sender
 {
     [self dismissViewControllerAnimated:YES completion:NULL];
 }
+
+// Let the puppy eat if bowl is full
+-(IBAction)eat:(id)sender
+{
+    if(fullBowl == YES)
+    {
+        [self animateEating];
+    
+        // Clear bowl after timer
+        [NSTimer scheduledTimerWithTimeInterval:3.0 target:self
+                                   selector:@selector(clearBowl:)
+                                   userInfo:nil repeats:NO];
+    }
+    else
+    {
+        // Create alert for not enough points to purchase
+        NSString *message = @"You must shake the screen to make the bowl full first";
+        
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Sorry!"
+                                                        message:message
+                                                       delegate:nil
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles: nil];
+        [alert show];
+    }
+}
+
+// Clear the bowl
+-(void)clearBowl:(NSTimer*)inTimer
+{
+    [inTimer invalidate];
+    inTimer = nil;
+    
+    bowlImageView.image = empty;
+    fullBowl = NO;
+    gIndex = 0;
+}
+
+
+// Create animation for puppy to appear to be getting ready to eat
+-(void) animateGoingToEat
+{
+    petImageView.animationImages =  [NSArray arrayWithObjects:
+                                     [UIImage imageNamed:@"goingToEat1.png"],
+                                     [UIImage imageNamed:@"goingToEat2.png"],
+                                     nil];
+    
+    petImageView.animationDuration = 1.0;
+    petImageView.animationRepeatCount = 1;
+    [petImageView startAnimating];
+    
+}
+
+// Create animation for puppy to appear to be eating
+-(void) animateEating
+{
+    petImageView.animationImages =  [NSArray arrayWithObjects:
+                                     [UIImage imageNamed:@"goingToEat1.png"],
+                                     [UIImage imageNamed:@"goingToEat2.png"],
+                                     [UIImage imageNamed:@"eating1.png"],
+                                     [UIImage imageNamed:@"eating2.png"],
+                                     [UIImage imageNamed:@"eating3.png"],
+                                     [UIImage imageNamed:@"eating4.png"],
+                                     [UIImage imageNamed:@"eating5.png"],
+                                     [UIImage imageNamed:@"eating6.png"],
+                                     [UIImage imageNamed:@"goingToEat2.png"],
+                                     [UIImage imageNamed:@"goingToEat1.png"],
+                                     nil];
+    
+    
+    petImageView.animationDuration = 3.0;
+    petImageView.animationRepeatCount = 1;
+    [petImageView startAnimating];
+}
+
+// Create animation for puppy to appear to stop eating
+-(void) animateStopEating
+{
+    petImageView.animationImages =  [NSArray arrayWithObjects:
+                                     [UIImage imageNamed:@"goingToEat2.png"],
+                                     [UIImage imageNamed:@"goingToEat1.png"],
+                                     nil];
+    
+    petImageView.animationDuration = 1.0;
+    petImageView.animationRepeatCount = 1;
+    [petImageView startAnimating];
+}
+
 
 @end
